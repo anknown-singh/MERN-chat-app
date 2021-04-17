09456f97359bbdb87f83ac7e101572aa3e50cf3c
ANknownSingh <57868617+anknown-singh@users.noreply.github.com>
2021-04-17 12:10:09 +0530

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -6,7 +6,7 @@ auth = {}
 auth.list = (req, res) => {
     userauth.find({uname: `${req.body.username}`}).exec((err, data)=>{
         // console.log(data[0].uname)
-        if (err) res.send({err})
+        if (err) res.send(err)
         else if (data.length !== 0 ) if (req.body.pword !== data[0].pword) {
             res.send('Wrong uname/password');
         } else res.send('login succesful')
